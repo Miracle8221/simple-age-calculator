01eb4521152c7ebc10984b6f377ebe4cead072bc
Miracle8221 <anagwumiracle098@gmail.com>
2023-07-29 21:59:00 +0100

    Add our project

---
+++ script.js
@@ -0,0 +1,19 @@
+let remainder = 11 % 3;
+
+console.log(remainder)
+
+let a = 2;
+let b = 3;
+let c = 2;
+a += 12;
+b -= 10
+console.log(b)
+
+const myDecimal = 5.7;
+// concantenating strings with plus operator
+const myStr = "This is the start. " + "This is the end.";
+console.log(myStr)
+// with the plus equals operator
+let myApple = "This is my first sentence. ";
+myApple += "This is my second sentence.";
+console.log(myApple)
