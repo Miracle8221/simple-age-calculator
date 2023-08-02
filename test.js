200c3d903a14a6cac6bdd5c3e13ac7f24d265395
Miracle8221 <anagwumiracle098@gmail.com>
2023-08-02 14:20:08 +0100

    array

---
+++ test.js
@@ -0,0 +1,11 @@
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const array = [21,22,23,24,25,26,27,28,29,30]
+
+
+for (let i = 1; i <= array.length; i++) {
+
+    if (array[i] % 2 === 0) {
+        console.log(array[i]);
+    }
+}
