3c768482d44c7cd34726b0c9ff822faf4c6e985f
Miracle8221 <anagwumiracle098@gmail.com>
2023-08-02 11:44:06 +0100

    create substraction to be return in the function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Creat variables to accept birth year and current year user input
 const a = parseInt(prompt("Enter birth year: "));
 const b = parseInt(prompt("Enter current year: "));
-// create an addition function
-// The addition function will return the addition for input a and b
+// create an substration function
+function substration() {
+    // The addition function will return the substraction for input a and b
+    return a - b;
+
+}
+
 // Invoke addition function to run the calculator
