71451e72c087c2048fd8599cd4aeb74c57188b10
Miracle8221 <anagwumiracle098@gmail.com>
2023-08-02 11:38:59 +0100

    create substration function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Creat variables to accept first and second user input
+// Creat variables to accept birth year and current year user input
+const a = parseInt(prompt("Enter birth year: "));
+const b = parseInt(prompt("Enter current year: "));
 // create an addition function
 // The addition function will return the addition for input a and b
 // Invoke addition function to run the calculator
