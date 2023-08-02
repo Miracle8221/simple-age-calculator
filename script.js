5054d2557509e17dbe1c8f8527ec8e1bbb137b4e
Miracle8221 <anagwumiracle098@gmail.com>
2023-08-02 12:41:29 +0100

    invoke function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Creat variables to accept birth year and current year user input
-const a = parseInt(prompt("Enter birth year: "));
-const b = parseInt(prompt("Enter current year: "));
+const birthYear = parseInt(prompt("Enter birth year: "));
+const currentYear = parseInt(prompt("Enter current year: "));
 // create an substration function
 function substration() {
-    // The substraction function will return the substraction for input a and b
-    return a - b;
+    // The substration function will return the substration for input a and b
+    return currentYear - birthYear;
 
 }
 
-// Invoke addition function to run the calculator
-console.log(substraction());
+// Invoke substration function to run the calculator
+console.log(substration());
