ba2b75c53f1aeb1754433770bff095e6e9f336df
Miracle8221 <anagwumiracle098@gmail.com>
2023-08-02 11:24:03 +0100

    create pseudocode for the calculator

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-let remainder = 11 % 3;
-
-console.log(remainder)
-
-let a = 2;
-let b = 3;
-let c = 2;
-a += 12;
-b -= 10
-console.log(b)
-
-const myDecimal = 5.7;
-// concantenating strings with plus operator
-const myStr = "This is the start. " + "This is the end.";
-console.log(myStr)
-// with the plus equals operator
-let myApple = "This is my first sentence. ";
-myApple += "This is my second sentence.";
-console.log(myApple)
+// Creat variables to accept first and second user input
+// create an addition function
+// The addition function will return the addition for input a and b
+// Invoke addition function to run the calculator
